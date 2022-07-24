a6e4eee9b3d5958a4df7f1a09fb84fe0099558cc
Bishal1998 <Bishalpaudel1998@gmail.com>
2022-07-24 21:53:55 +0545

    social link added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Social from './components/Social';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
+      <Social />
     </div>
   )
 }
