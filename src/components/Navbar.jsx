5ff1616d4a142d67a1ae24c802a42c99c7302ee9
Bishal1998 <Bishalpaudel1998@gmail.com>
2022-07-24 20:35:53 +0545

    navbar --responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
 
     const links = [
         {
@@ -24,7 +27,7 @@ const Navbar = () => {
     return (
         <div className='flex justify-between items-center px-4 text-white bg-black w-full h-20'>
             <div className='text-5xl font-signature mt-2 container'>Bishal</div>
-            <ul className='flex'>
+            <ul className='hidden md:flex'>
                 {
                     links.map((link, i) => {
                         return (
@@ -32,9 +35,24 @@ const Navbar = () => {
                         )
                     })
                 }
-
-
             </ul>
+            <div className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden" onClick={() => setNav(!nav)}>
+                {
+                    nav ? <FaTimes size={30} /> : <FaBars size={30} />
+                }
+            </div>
+            {
+                nav &&
+                <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 ">
+                    {
+                        links.map((link, i) => {
+                            return (
+                                <li className='px-4 cursor-pointer capitalize py-6 text-4xl' key={i}>{link.link}</li>
+                            )
+                        })
+                    }
+                </ul>
+            }
         </div>
     )
 }
